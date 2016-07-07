7a7b01148cc61a985c64de49eafc6f1d8cb3cc74
calphool <jrounceville@hotmail.com>
2016-07-07 00:21:59 -0500

    Refactored RNH class... smaller? yeah... easier to maintain? meh.

--- RomanNumerals/src/com/rounceville/RomanNumeralHelper.java
+++ RomanNumerals/src/com/rounceville/RomanNumeralHelper.java
@@ -9,61 +9,7 @@ import java.util.regex.*;
 
 public class RomanNumeralHelper {
 
-
-
-	public Object toInt(String inp) throws ParseException {
-		int iTotal = 0;
-		inp = inp.toUpperCase();
-		
-		validateRomanNumeralInput(inp);
-		
-		for(int i=0; i < inp.length(); i++) {
-			int cVal = charToInt(inp.charAt(i));
-			if(i < inp.length() - 1) {
-				int cVal2 = charToInt(inp.charAt(i+1));
-				boolean bDoSubtraction = true;
-				if(cVal == 5 || cVal == 50 || cVal == 500)
-					bDoSubtraction = false;
-				if(cVal == 1 && cVal2 > 10)
-					bDoSubtraction = false;
-				if(cVal == 5 && cVal2 > 50)
-					bDoSubtraction = false;
-				if(bDoSubtraction && (cVal2 > cVal)) {
-					iTotal += cVal2;
-					cVal = -cVal;
-					i++;
-				}				
-			}
-			
-			iTotal += cVal;
-		}
-		
-		return new Integer(iTotal);
-	}
-	
-	private void validateRomanNumeralInput(String inp) throws ParseException {		
-		// http://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
-		if(!Pattern.matches("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",inp)) 
-			throw new ParseException("Unknown problem with your roman numeral input: " + inp, -1);
-	}
-
-	private int charToInt(char c) throws ParseException {
-		switch(c) {
-			case 'M': return 1000;
-			case 'D': return 500;
-			case 'C': return 100;
-			case 'L': return 50;
-			case 'X': return 10;
-			case 'V': return 5;
-			case 'I': return 1;
-			default: throw new ParseException("Unknown roman numeral: " + c, -1); 
-		}
-	}
-	
-
-	public String toNumeral(int i) {
-		StringBuilder sb = new StringBuilder();
-		
+	private Queue<AbstractMap.SimpleEntry<Integer,String>> getRomanNumeralValueQueue() {
 		Queue<AbstractMap.SimpleEntry<Integer,String>> q = new LinkedList<AbstractMap.SimpleEntry<Integer,String>>();
 		q.add(new SimpleEntry<Integer, String>(1000,"M"));
 		q.add(new SimpleEntry<Integer, String>(900,"CM"));
@@ -79,11 +25,50 @@ public class RomanNumeralHelper {
 		q.add(new SimpleEntry<Integer, String>(4,"IV"));
 		q.add(new SimpleEntry<Integer, String>(1,"I"));
 
-		while(!q.isEmpty()) {
-			SimpleEntry<Integer, String> qItem = q.poll();
-			while(i >= qItem.getKey()) {
-				sb.append(qItem.getValue());
-				i = i - qItem.getKey();
+		return q;
+	}
+
+	
+	public Object toInt(String inp) throws ParseException {
+		int iTotal = 0;
+		inp = inp.toUpperCase();
+		
+		validateRomanNumeralInput(inp);
+		
+		Queue<AbstractMap.SimpleEntry<Integer,String>> q = getRomanNumeralValueQueue();
+	
+		while(inp.length() > 0) {                                   // while we still have a string to work with
+			SimpleEntry<Integer, String> se = q.poll();				// pull queue item off
+			String sNode = se.getValue();							// what letter are we on?
+			String sInputWindow = inp.substring(0, Math.min(sNode.length(), inp.length()));  // window the input string
+			while(sInputWindow.equalsIgnoreCase(sNode)) {           // while the window still matches the queue letter(s)
+				iTotal += se.getKey();								// add this letter(s) to the total
+				inp = inp.substring(sNode.length());				// advance the input string forward
+				sInputWindow = inp.substring(0, Math.min(sNode.length(), inp.length()));  // determine new window value
+			}
+		}
+
+		return new Integer(iTotal);
+	}
+	
+	private void validateRomanNumeralInput(String inp) throws ParseException {		
+		// http://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
+		if(!Pattern.matches("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",inp)) 
+			throw new ParseException("Unknown problem with your roman numeral input: " + inp, -1);
+	}
+	
+	
+
+	public String toNumeral(int i) {
+		StringBuilder sb = new StringBuilder();
+		
+		Queue<AbstractMap.SimpleEntry<Integer,String>> q = getRomanNumeralValueQueue();
+		
+		while(!q.isEmpty()) {                              // while we still have letters to check
+			SimpleEntry<Integer, String> qItem = q.poll(); // pull the next one off
+			while(i >= qItem.getKey()) {                   // while our input value exceeds the value of this letter
+				sb.append(qItem.getValue());               // append this letter to our output string
+				i = i - qItem.getKey();                    // subtract this letter's value from running decrementer
 			}
 		}
 		
