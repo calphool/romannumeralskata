2ac95028867e87921fd54ab70a8ba8bd623a4666
calphool <jrounceville@hotmail.com>
2016-07-08 22:52:29 -0500

    Added better exhaustive test, cleaned up variable names

--- RomanNumerals/src/com/rounceville/RomanNumeralHelper.java
+++ RomanNumerals/src/com/rounceville/RomanNumeralHelper.java
@@ -9,6 +9,12 @@ import java.util.regex.*;
 
 public class RomanNumeralHelper {
 
+	/**
+	 * Returns a queue containing Arabic integers and their associated Roman
+	 * Numeral primitives. 
+	 *
+	 * @return      Queue of entries
+	 */
 	private Queue<AbstractMap.SimpleEntry<Integer,String>> getRomanNumeralValueQueue() {
 		Queue<AbstractMap.SimpleEntry<Integer,String>> q = new LinkedList<AbstractMap.SimpleEntry<Integer,String>>();
 		q.add(new SimpleEntry<Integer, String>(1000,"M"));
@@ -29,53 +35,63 @@ public class RomanNumeralHelper {
 	}
 
 	
-	public Object toInt(String inp) throws ParseException {
-		int iTotal = 0;
-		inp = inp.toUpperCase();
+	public Object toArabic(String sRomanInput) throws ParseException {
+		int iReturnValue = 0;
+		sRomanInput = sRomanInput.toUpperCase();
 		
-		validateRomanNumeralInput(inp);
+		validateRomanNumeralInput(sRomanInput);
 		
-		Queue<AbstractMap.SimpleEntry<Integer,String>> q = getRomanNumeralValueQueue();
+		Queue<AbstractMap.SimpleEntry<Integer,String>> qRomanAndArabic = getRomanNumeralValueQueue();
 	
-		while(inp.length() > 0) {                                   // while we still have a string to work with
-			SimpleEntry<Integer, String> se = q.poll();				// pull queue item off
-			String sNode = se.getValue();							// what letter are we on?
-			String sInputWindow = inp.substring(0, Math.min(sNode.length(), inp.length()));  // window the input string
-			while(sInputWindow.equalsIgnoreCase(sNode)) {           // while the window still matches the queue letter(s)
-				iTotal += se.getKey();								// add this letter(s) to the total
-				inp = inp.substring(sNode.length());				// advance the input string forward
-				sInputWindow = inp.substring(0, Math.min(sNode.length(), inp.length()));  // determine new window value
+		while(sRomanInput.length() > 0) {                                   	// while we still have a string to work with
+			SimpleEntry<Integer, String> qEntry = qRomanAndArabic.poll();		// pull queue item off
+			String sNode = qEntry.getValue();									// what letter are we on?
+			String sInputWindow = sRomanInput.substring(0, Math.min(sNode.length(), sRomanInput.length()));  // window the input string
+			while(sInputWindow.equalsIgnoreCase(sNode)) {           			// while the window still matches the queue letter(s)
+				iReturnValue += qEntry.getKey();								// add this letter(s) to the total
+				sRomanInput = sRomanInput.substring(sNode.length());			// advance the input string forward
+				sInputWindow = sRomanInput.substring(0, Math.min(sNode.length(), sRomanInput.length()));  // determine new window value
 			}
 		}
 
-		return new Integer(iTotal);
+		return new Integer(iReturnValue);
 	}
 	
-	private void validateRomanNumeralInput(String inp) throws ParseException {		
+	private void validateRomanNumeralInput(String sStringToTest) throws ParseException {
+		// originally started building up a list of regular expressions here...
+		// decided it would be better to find a comprehensive one
+		
+		// Found:
 		// http://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
-		if(!Pattern.matches("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",inp)) 
-			throw new ParseException("Unknown problem with your roman numeral input: " + inp, -1);
+		/*
+		 * This regular expression translated into English:
+		 * starting at the beginning, zero to three M's followed by a choice of
+		 * CM, CD, D or (zero to 3 Cs) followed by XC, XL, L, or (zero to 3 Xs)
+		 * followed by IX, IV, V, or zero to 3 I's, and that's it
+		 */
+		
+		if(!Pattern.matches("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",sStringToTest))   
+			throw new ParseException("Unknown problem with your roman numeral input: " + sStringToTest, -1);
 	}
 	
 	
 
-	public String toNumeral(int i) {
-		if(i < 1 || i > 3999)
+	public String toRomanNumeral(int iArabicNumber) {
+		if(iArabicNumber < 1 || iArabicNumber > 3999)
 			throw new IllegalArgumentException("Values must be in the range 1 - 3999 inclusive.");
 		
+		StringBuilder sReturnStringBuilder = new StringBuilder();
 		
-		StringBuilder sb = new StringBuilder();
+		Queue<AbstractMap.SimpleEntry<Integer,String>> qRomanAndArabic = getRomanNumeralValueQueue();
 		
-		Queue<AbstractMap.SimpleEntry<Integer,String>> q = getRomanNumeralValueQueue();
-		
-		while(!q.isEmpty()) {                              // while we still have letters to check
-			SimpleEntry<Integer, String> qItem = q.poll(); // pull the next one off
-			while(i >= qItem.getKey()) {                   // while our input value exceeds the value of this letter
-				sb.append(qItem.getValue());               // append this letter to our output string
-				i = i - qItem.getKey();                    // subtract this letter's value from running decrementer
+		while(!qRomanAndArabic.isEmpty()) {                                     // while we still have letters to check
+			SimpleEntry<Integer, String> qEntry = qRomanAndArabic.poll();       // pull the next one off
+			while(iArabicNumber >= qEntry.getKey()) {                          	// while our input value exceeds the value of this letter
+				sReturnStringBuilder.append(qEntry.getValue());    				// append this letter to our output string
+				iArabicNumber = iArabicNumber - qEntry.getKey();     			// subtract this letter's value from running decrementer
 			}
 		}
 		
-		return sb.toString();
+		return sReturnStringBuilder.toString();
 	}
 }
--- RomanNumerals/src/com/rounceville/RomanNumeralsTest.java
+++ RomanNumerals/src/com/rounceville/RomanNumeralsTest.java
@@ -21,159 +21,213 @@ public class RomanNumeralsTest {
 	    rnh = new RomanNumeralHelper();
 	}
 	
+	
 	@Test
 	public void testPrimitivesLetterToInt() throws ParseException {
-		assertEquals(rnh.toInt("M"), 1000);
-		assertEquals(rnh.toInt("D"), 500);
-		assertEquals(rnh.toInt("C"), 100);
-		assertEquals(rnh.toInt("L"), 50);		
-		assertEquals(rnh.toInt("X"), 10);		
-		assertEquals(rnh.toInt("V"), 5);
-		assertEquals(rnh.toInt("I"), 1);
+		// prove primitives work
+		assertEquals(rnh.toArabic("M"), 1000);
+		assertEquals(rnh.toArabic("D"), 500);
+		assertEquals(rnh.toArabic("C"), 100);
+		assertEquals(rnh.toArabic("L"), 50);		
+		assertEquals(rnh.toArabic("X"), 10);		
+		assertEquals(rnh.toArabic("V"), 5);
+		assertEquals(rnh.toArabic("I"), 1);
 	}
+	
 	
 	@Test
 	public void testTripleRepeats() throws ParseException {
-		assertEquals(rnh.toInt("III"),3);
-		assertEquals(rnh.toInt("XXX"),30);
-		assertEquals(rnh.toInt("CCC"),300);
-		assertEquals(rnh.toInt("MMM"),3000);
+		// prove triples work
+		assertEquals(rnh.toArabic("III"),3);
+		assertEquals(rnh.toArabic("XXX"),30);
+		assertEquals(rnh.toArabic("CCC"),300);
+		assertEquals(rnh.toArabic("MMM"),3000);
 	}
+	
 	
 	@Test
 	public void testQuadrupleIIIIRepeatFails() throws ParseException {
+		// No quadruples 
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("IIII"),4);
+		assertNotEquals(rnh.toArabic("IIII"),4);
 	}
+	
 	
 	@Test
 	public void testQuadrupleXXXXRepeatFails() throws ParseException {
+		// No quadruples 
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("XXXX"),40);
+		assertNotEquals(rnh.toArabic("XXXX"),40);
 	}
+	
 	
 	@Test
 	public void testQuadrupleCCCCRepeatFails() throws ParseException {
+		// No quadruples 
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("CCCC"),400);
+		assertNotEquals(rnh.toArabic("CCCC"),400);
 	}
+	
 	
 	@Test
 	public void testQuadrupleMMMMRepeatFails() throws ParseException {
+		// No quadruples 
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("MMMM"),4000);
+		assertNotEquals(rnh.toArabic("MMMM"),4000);
 	}
 
+	
 	@Test
 	public void testQuadrupleMIIIIRepeatFails() throws ParseException {
+		// No quadruples 
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("MIIII"),1004);
+		assertNotEquals(rnh.toArabic("MIIII"),1004);
 	}
+	
 	
 	@Test
 	public void testRepeatingVFails() throws ParseException {
+		// V's cannot be doubled
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("VV"),10);
+		assertNotEquals(rnh.toArabic("VV"),10);
 	}
 
+	
 	@Test
 	public void testRepeatingLFails() throws ParseException {
+		// L's cannot be doubled
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("LL"),100);
+		assertNotEquals(rnh.toArabic("LL"),100);
 	}
 
+	
 	@Test
 	public void testRepeatingDFails() throws ParseException {
+		// D's cannot be doubled
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("DD"),1000);
+		assertNotEquals(rnh.toArabic("DD"),1000);
 	}
+	
 	
 	@Test
 	public void testRepeatingMDDFails() throws ParseException {
+		// D's cannot be doubled
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("MDD"),2000);
+		assertNotEquals(rnh.toArabic("MDD"),2000);
 	}
+	
 	
 	@Test
 	public void testIVandIXandXLandXCandCDandCM() throws ParseException {
-		assertEquals(rnh.toInt("IV"), 4);
-		assertEquals(rnh.toInt("IX"), 9);
-		assertEquals(rnh.toInt("XC"), 90);
-		assertEquals(rnh.toInt("CD"), 400);
-		assertEquals(rnh.toInt("CM"), 900);
+		// specific valid tests for subtraction
+		assertEquals(rnh.toArabic("IV"), 4);
+		assertEquals(rnh.toArabic("IX"), 9);
+		assertEquals(rnh.toArabic("XC"), 90);
+		assertEquals(rnh.toArabic("CD"), 400);
+		assertEquals(rnh.toArabic("CM"), 900);
 	}
 
+	
 	@Test
 	public void testILandICandIDandIM() throws ParseException {
+		// specific invalid tests (I is too far away from L to subtract for example)
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("IL"), 49);
-		assertNotEquals(rnh.toInt("IC"), 99);
-		assertNotEquals(rnh.toInt("ID"), 499);
-		assertNotEquals(rnh.toInt("IM"), 999);
+		assertNotEquals(rnh.toArabic("IL"), 49);
+		assertNotEquals(rnh.toArabic("IC"), 99);
+		assertNotEquals(rnh.toArabic("ID"), 499);
+		assertNotEquals(rnh.toArabic("IM"), 999);
 	}
 
 	@Test
 	public void testNoDoubleSubtractions() throws ParseException {
+		// double subtraction isn't allowed
 		exception.expect(ParseException.class);
-		assertNotEquals(rnh.toInt("XXC"), 80);
+		assertNotEquals(rnh.toArabic("XXC"), 80);
 	}
 
+	
 	@Test
 	public void testGivenFeature1TestCases() throws ParseException {
-		assertEquals(rnh.toInt("I"), 1);
-		assertEquals(rnh.toInt("III"), 3);
-		assertEquals(rnh.toInt("IX"), 9);
-		assertEquals(rnh.toInt("MLXVI"), 1066);
-		assertEquals(rnh.toInt("MCMLXXXIX"), 1989);
+		// these are required tests for feature 1
+		assertEquals(rnh.toArabic("I"), 1);
+		assertEquals(rnh.toArabic("III"), 3);
+		assertEquals(rnh.toArabic("IX"), 9);
+		assertEquals(rnh.toArabic("MLXVI"), 1066);
+		assertEquals(rnh.toArabic("MCMLXXXIX"), 1989);
 	}
+	
 	
 	@Test
 	public void testPrimitiveIntToLetter() {
-		assertEquals(rnh.toNumeral(1000), "M");
-		assertEquals(rnh.toNumeral(500), "D");
-		assertEquals(rnh.toNumeral(100), "C");
-		assertEquals(rnh.toNumeral(50), "L");
-		assertEquals(rnh.toNumeral(10), "X");
-		assertEquals(rnh.toNumeral(5), "V");
-		assertEquals(rnh.toNumeral(1), "I");
+		// these are primitive tests
+		assertEquals(rnh.toRomanNumeral(1000), "M");
+		assertEquals(rnh.toRomanNumeral(500), "D");
+		assertEquals(rnh.toRomanNumeral(100), "C");
+		assertEquals(rnh.toRomanNumeral(50), "L");
+		assertEquals(rnh.toRomanNumeral(10), "X");
+		assertEquals(rnh.toRomanNumeral(5), "V");
+		assertEquals(rnh.toRomanNumeral(1), "I");
 	}
+	
 	
 	@Test
 	public void testGivenFeature2TestCases() {
-		assertEquals(rnh.toNumeral(1), "I");
-		assertEquals(rnh.toNumeral(3), "III");
-		assertEquals(rnh.toNumeral(9), "IX");
-		assertEquals(rnh.toNumeral(1066), "MLXVI");
-		assertEquals(rnh.toNumeral(1989), "MCMLXXXIX");
+		// these were required test cases for feature 2
+		assertEquals(rnh.toRomanNumeral(1), "I");
+		assertEquals(rnh.toRomanNumeral(3), "III");
+		assertEquals(rnh.toRomanNumeral(9), "IX");
+		assertEquals(rnh.toRomanNumeral(1066), "MLXVI");
+		assertEquals(rnh.toRomanNumeral(1989), "MCMLXXXIX");
 	}
+	
 	
 	@Test
 	public void testNegativeNumbers() throws IllegalArgumentException {
+		// no negative numbers
 		exception.expect(IllegalArgumentException.class);
-		rnh.toNumeral(-1);
+		rnh.toRomanNumeral(-1);
 	}
+	
 	
 	@Test 
 	public void testZeroNumber() throws IllegalArgumentException {
+		// no zeros
 		exception.expect(IllegalArgumentException.class);
-		rnh.toNumeral(0);
+		rnh.toRomanNumeral(0);
 	}
+	
 	
 	@Test
 	public void testLargeNumber() throws IllegalArgumentException {
+		// no numbers larger than 3999
 		exception.expect(IllegalArgumentException.class);
-		rnh.toNumeral(4000);
+		rnh.toRomanNumeral(4000);
 	}
 
 	
 	@Test
-	public void testCrossCheckExhaustively() throws ParseException {
-		for(int i=1;i<4000;i++) {
-			String sNumeral = rnh.toNumeral(i);
-			assertEquals(i, rnh.toInt(sNumeral));
+	public void testLessControversialExhaustiveTest() throws ParseException {
+		// list generated from excel... since the range is limited (1-3999), we can 
+		// literally test every single value for validity
+
+		String sList = RomanNumeralList.getFullRomanNumeralTable();
+		
+		String[] sLines = sList.split("\n");
+		for(String sLine : sLines) {
+			String sKey = sLine.split(":")[0];
+			String sValue = sLine.split(":")[1];
+			assertEquals(rnh.toRomanNumeral(Integer.parseInt(sKey)), sValue);
+			assertEquals(rnh.toArabic(sValue), Integer.parseInt(sKey));
 		}
 	}
 	
 	
-	
+	@Test
+	public void testCrossCheckExhaustively() throws ParseException {
+		// run every number through the helper class (arabic int to roman and roman back to arabic int)
+		for(int i=1;i<4000;i++) {
+			String sNumeral = rnh.toRomanNumeral(i);
+			assertEquals(i, rnh.toArabic(sNumeral));
+		}
+	}
 }
